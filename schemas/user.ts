05d2703e7ed2e87252b61780e6a5b65d6203763f
skiller70 <singhlucky007007@gmail.com>
2023-10-30 13:47:42 +0530

    added user schema

---
+++ schemas/user.ts
@@ -0,0 +1,29 @@
+
+
+export default {
+    name: 'user',
+    title: 'User',
+    type: 'document',
+    fields: [
+      {
+        name: 'username',
+        title: 'Username',
+        type: 'string',
+      },
+      {
+        name: 'email',
+        title: 'Email',
+        type: 'string',
+      },
+      {
+        name: 'avatar',
+        title: 'Avatar',
+        type: 'image',
+      },
+      {
+        name: 'bio',
+        title: 'Bio',
+        type: 'text',
+      },
+    ],
+  };
