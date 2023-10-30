4d184d9dd2530a75524ecfcf9981fd1b0b9740bc
skiller70 <singhlucky007007@gmail.com>
2023-10-30 15:40:21 +0530

    testing arcanas

--- schemas/arcana.ts
+++ schemas/arcana.ts
@@ -1,22 +1,42 @@
 export default {
-    name: 'arcanas',
-    title: 'Arcana',
-    type: 'document',
-    fields: [
-      {
-        name: 'name',
-        title: 'addArcana',
-        type: 'string',
+  name: 'arcanas',
+  title: 'Arcana',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'addArcana',
+      type: 'string',
+    },
+    {
+      title: 'Launch Scheduled At',
+      name: 'launchAt',
+      type: 'datetime',
+    },
+    {
+      title: 'Arcana Price',
+      name: 'price',
+      type: 'number',
+    },
+    {
+      title: 'Poster',
+      name: 'poster',
+      type: 'image',
+      options: {
+        hotspot: true, // <-- Defaults to false
       },
-      {
-        title: 'Launch Scheduled At',
-        name: 'launchAt',
-        type: 'datetime'
-      },
-      {
-        title: 'Arcana Price',
-        name: 'price',
-        type: 'number'
-      }
-    ],
-  };
+      fields: [
+        {
+          name: 'caption',
+          type: 'string',
+          title: 'Caption',
+        },
+        {
+          name: 'attribution',
+          type: 'string',
+          title: 'Attribution',
+        },
+      ],
+    },
+  ],
+}
