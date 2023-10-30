259ec6dc37dfec7e1a6b8cd8336907b6e6b34109
skiller70 <singhlucky007007@gmail.com>
2023-10-30 14:48:05 +0530

    added hero

---
+++ schemas/heros.ts
@@ -0,0 +1,19 @@
+export default {
+    name: 'heros',
+    title: 'Heros',
+    type: 'document',
+    fields: [
+      {
+        name: 'hero',
+        title: 'Add Hero',
+        type: 'string',
+      },
+    
+      {
+        title: 'Launch Scheduled At',
+        name: 'launchAt',
+        type: 'datetime'
+      },
+      
+    ],
+  };
